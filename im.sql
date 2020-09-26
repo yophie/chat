b499396fb3da9dfdbb9de4966911a585aecfca5c
leo <lg0927@hotmail.com>
2020-09-26 17:40:38 +0800

    add sql

--- im.sql
+++ im.sql
@@ -16,27 +16,200 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `group_users`
+-- Table structure for table `cashout`
 --
 
-DROP TABLE IF EXISTS `group_users`;
+DROP TABLE IF EXISTS `cashout`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `group_users` (
-  `gid` int NOT NULL,
-  `uid` int NOT NULL,
-  PRIMARY KEY (`gid`,`uid`)
+CREATE TABLE `cashout` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `userId` varchar(50) NOT NULL,
+  `state` int DEFAULT NULL,
+  `amount` decimal(20,2) DEFAULT NULL,
+  `applyTime` bigint DEFAULT NULL,
+  `approvalTime` bigint DEFAULT NULL,
+  `fee` decimal(20,4) DEFAULT NULL,
+  `approvalAmount` decimal(20,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `group_users`
+-- Dumping data for table `cashout`
 --
 
-LOCK TABLES `group_users` WRITE;
-/*!40000 ALTER TABLE `group_users` DISABLE KEYS */;
-INSERT INTO `group_users` VALUES (1,2),(1,3),(2,1),(2,2),(2,3),(3,1),(4,1);
-/*!40000 ALTER TABLE `group_users` ENABLE KEYS */;
+LOCK TABLES `cashout` WRITE;
+/*!40000 ALTER TABLE `cashout` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cashout` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `chatGroup`
+--
+
+DROP TABLE IF EXISTS `chatGroup`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `chatGroup` (
+  `groupId` int NOT NULL,
+  `groupName` varchar(50) DEFAULT NULL,
+  `avatar` varchar(50) DEFAULT NULL,
+  `owner` int NOT NULL,
+  PRIMARY KEY (`groupId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `chatGroup`
+--
+
+LOCK TABLES `chatGroup` WRITE;
+/*!40000 ALTER TABLE `chatGroup` DISABLE KEYS */;
+INSERT INTO `chatGroup` VALUES (13,'我的群聊','test avatar',8),(14,'我的群聊','test avatar',8);
+/*!40000 ALTER TABLE `chatGroup` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `chatWindow`
+--
+
+DROP TABLE IF EXISTS `chatWindow`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `chatWindow` (
+  `chatId` int NOT NULL AUTO_INCREMENT,
+  `windowName` varchar(50) DEFAULT NULL,
+  `owner` int NOT NULL,
+  `userGroupId` int NOT NULL,
+  `lastMessage` varchar(50) DEFAULT NULL,
+  `lastTime` bigint DEFAULT NULL,
+  `chatType` int NOT NULL,
+  `unReadNum` int NOT NULL,
+  `lastUserId` int DEFAULT NULL,
+  `lastName` varchar(50) DEFAULT NULL,
+  `lastAvatar` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`chatId`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `chatWindow`
+--
+
+LOCK TABLES `chatWindow` WRITE;
+/*!40000 ALTER TABLE `chatWindow` DISABLE KEYS */;
+/*!40000 ALTER TABLE `chatWindow` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `friend`
+--
+
+DROP TABLE IF EXISTS `friend`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `friend` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `myId` int NOT NULL,
+  `friendId` int NOT NULL,
+  `friendNick` varchar(50) DEFAULT NULL,
+  `state` int NOT NULL,
+  `applyTime` bigint NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `friend`
+--
+
+LOCK TABLES `friend` WRITE;
+/*!40000 ALTER TABLE `friend` DISABLE KEYS */;
+INSERT INTO `friend` VALUES (7,8,9,NULL,1,1601097444126),(8,9,8,NULL,1,1601097444126),(10,9,13,'我的群聊',0,1601105897205),(11,8,14,'我的群聊',0,1601107650404),(12,9,14,'我的群聊',0,1601107650404);
+/*!40000 ALTER TABLE `friend` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `friendSociety`
+--
+
+DROP TABLE IF EXISTS `friendSociety`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `friendSociety` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `userId` int NOT NULL,
+  `time` bigint DEFAULT NULL,
+  `content` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `friendSociety`
+--
+
+LOCK TABLES `friendSociety` WRITE;
+/*!40000 ALTER TABLE `friendSociety` DISABLE KEYS */;
+/*!40000 ALTER TABLE `friendSociety` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `packet`
+--
+
+DROP TABLE IF EXISTS `packet`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `packet` (
+  `id` varchar(50) NOT NULL,
+  `type` int DEFAULT NULL,
+  `amount` decimal(20,2) DEFAULT NULL,
+  `userGroupId` int DEFAULT NULL,
+  `lastMessage` varchar(50) DEFAULT NULL,
+  `num` int DEFAULT NULL,
+  `time` bigint DEFAULT NULL,
+  `state` int DEFAULT NULL,
+  `returnAmount` int DEFAULT NULL,
+  `returnTime` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `packet`
+--
+
+LOCK TABLES `packet` WRITE;
+/*!40000 ALTER TABLE `packet` DISABLE KEYS */;
+/*!40000 ALTER TABLE `packet` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `packetState`
+--
+
+DROP TABLE IF EXISTS `packetState`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `packetState` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `packetId` varchar(50) NOT NULL,
+  `reciever` int DEFAULT NULL,
+  `amount` decimal(20,2) DEFAULT NULL,
+  `time` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `packetState`
+--
+
+LOCK TABLES `packetState` WRITE;
+/*!40000 ALTER TABLE `packetState` DISABLE KEYS */;
+/*!40000 ALTER TABLE `packetState` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -48,14 +221,15 @@ DROP TABLE IF EXISTS `user`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) DEFAULT NULL,
+  `account` varchar(50) DEFAULT NULL,
   `openId` varchar(50) DEFAULT NULL,
-  `unionId` varchar(50) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `avatar` varchar(50) DEFAULT NULL,
+  `phone` varchar(50) DEFAULT NULL,
   `password` varchar(50) DEFAULT NULL,
-  `avatar` varchar(200) DEFAULT NULL,
-  `friendGroup` int DEFAULT NULL,
+  `money` decimal(20,2) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -64,33 +238,8 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'test','test',NULL,'098f6bcd4621d373cade4e832627b4f6',NULL,NULL),(2,'test2','test2',NULL,'098f6bcd4621d373cade4e832627b4f6',NULL,NULL),(3,'test3','test3',NULL,'098f6bcd4621d373cade4e832627b4f6',NULL,NULL);
+INSERT INTO `user` VALUES (4,'testaccount','testopenid','testname','testavatar','13388888888','test',8888.88),(8,'66d4591207034c41bbe62cd3a056e3d1','test1','newUser',NULL,NULL,'test',NULL),(9,'e9c4231e74c246f9a06a7819a3aca303','test2','newUser',NULL,NULL,'test',NULL),(11,NULL,'9','我的群聊','test avatar',NULL,'group',NULL),(12,NULL,'9','我的群聊','test avatar',NULL,'group',NULL),(13,'e71c9fe908f74956818c3e22036164c3','8','我的群聊','test avatar',NULL,'group',NULL),(14,'b2c23300cdcb437da15cb5c1db43aaaa','8','我的群聊','test avatar',NULL,'group',NULL);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user_group`
---
-
-DROP TABLE IF EXISTS `user_group`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user_group` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) DEFAULT NULL,
-  `owner` int DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user_group`
---
-
-LOCK TABLES `user_group` WRITE;
-/*!40000 ALTER TABLE `user_group` DISABLE KEYS */;
-INSERT INTO `user_group` VALUES (1,'test1\'s friends',1),(2,'group chat',0),(3,'test2\'s friends',2),(4,'test3\'s friends',3);
-/*!40000 ALTER TABLE `user_group` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -102,4 +251,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-09-22 15:36:29
+-- Dump completed on 2020-09-26 17:40:09
